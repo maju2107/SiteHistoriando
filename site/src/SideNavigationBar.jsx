3e0b289c7b156b9150ab861ff58cd7d7e26addf9
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-03-30 11:46:28 -0300

    [update} Side bar apenas com icons
    Sider par com icons e title

--- site/src/SideNavigationBar.jsx
+++ site/src/SideNavigationBar.jsx
@@ -2,23 +2,24 @@ import React, {useState} from 'react';
 import './SideNavigationBar.css';
 
 
-
 function SideNavigationBar() {
 
     const [open, setOpen] = useState(false);
 
     return (
         <>
-        <div className='sidenav' style={{width: open? "250px" : "0"}}>
-            <a className='closebtn' onClick={() => setOpen(false)}>&times;</a>
-            <a href='#'>Pesquisar</a>
-            <a href='#'>Perguntas</a>
-            <a href='#'>Conteúdos</a>
-            <a href='#'>Mapa Mental</a>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
+        <div className='sidenav' /*style={{width: open? "250px" : "0"}}*/ >
+            {/*<a className='closebtn' onClick={() => setOpen(false)}>&times;</a>*/}
+            <a href='#'><i className ="fa fa-fw fa-search" title='Pesquisar'></i></a>
+            <a href='#'><i className ="fa fa-fw fa-question" title='Perguntas'></i></a>
+            <a href='#'><i className ="fa fa-fw fa-book" title='Conteúdo'></i></a>
+            <a href='#'><i className ="fa fa-fw fa-map" title='Mapa Mental'></i></a>
         </div>
 
-        <div id='main' style={{marginLeft: open? "250px" : "0"}} >
-            <span style={{fontSize:"30px", cursor:"pointer", position:"absolute", left:"0"}} onClick={() => setOpen(true)}>&#9776;</span>
+        <div id='main' /*style={{marginLeft: open? "250px" : "0"}}*/ >
+            {/*<span style={{fontSize:"30px", cursor:"pointer", position:"absolute", left:"0"}} onClick={() => setOpen(true)}>&#9776;</span>*/}
             <h2>Bem vindo!</h2>
             <p>Para aprender mais, sempre pesquise!</p>
             
