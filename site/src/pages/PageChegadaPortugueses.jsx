ab45912083718f89b277a457341badb6e4d7c64a
maju2107 <maju.mariajuliaaraujo@gmail.com>
2026-05-13 17:53:32 -0300

    [create] Adicionado texto a PageChegadaPortugueses
    Criei uma pasta data para ter os arquivos de texto em formato json

--- site/src/pages/PageChegadaPortugueses.jsx
+++ site/src/pages/PageChegadaPortugueses.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import '../css/PageChegadaPortugueses.css';
 
+import content from '../data/textChegadaPortugueses.json';
+
 function PageChegadaPortugueses() {
 
     return (
         <>
-        <h1> A CHEGADA DOS PORTUGUESES AO BRASIL </h1>
+        <h1> {content.title} </h1>
 
-        <div className='paragraph'>
-            <p> Oficialmente, a chegada dos  portugueses ao Brasil ocorreu com <br></br>a expedição liderada por Pedro Álvares Cabral, composta por treze caravelas <br></br>que partiram de Lisboa no dia 9 de março de 1500. Essa expedição tinha como destino final a Índia, porém, durante o trajeto, desviou-se do caminho original e acabou alcançando o Brasil, na região de Porto Seguro, em 22 de abril de 1500. </p>
-        </div>
-
-        <div className='paragraph'>
-            <p>Existe um certo debate a respeito da intencionalidade da expedição de Pedro Álvares Cabral em sua vinda à América. Alguns historiadores afirmam que existia a intenção de Cabral em desviar-se do trajeto original. Outros especialistas, porém, discordam dessa ideia e afirmam que a chegada dos portugueses ao Brasil foi um evento ocasional.</p>
-        </div>
+        {content.paragraphs.map( (paragraph) => (
+            <div className='paragraph' key={paragraph.id}>
+                <p>{paragraph.text}</p>
+            </div>
+        ))}
         </>
     )
 }
