f20b74d17e7e6ebcb0c3171c83c07fdb3d56d98b
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-06-15 11:59:25 -0300

    [update] SidebarNavigation responsiva
    Em dimensões mobile (<575) a navbar se torna um toggler( btn sanduíche )

--- site/src/components/SideNavigationBar.jsx
+++ site/src/components/SideNavigationBar.jsx
@@ -3,25 +3,100 @@ import '../css/SideNavigationBar.css';
 import { Link } from 'react-router-dom';
 
 function SideNavigationBar() {
-
-    // const [open, setOpen] = useState(false);
-
     return (
         <>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+            />
 
-        <div className='sidenav' /*style={{width: open? "250px" : "0"}}*/ >
-            {/*<a className='closebtn' onClick={() => setOpen(false)}>&times;</a>*/}
-            <Link to="/"><i className ="fa fa-fw fa-home" title='Home'></i></Link>
-            <a href='#'><i className ="fa fa-fw fa-search" title='Pesquisar'></i></a>
-            <a href='#'><i className ="fa fa-fw fa-question" title='Perguntas'></i></a>
-            <a href='#'><i className ="fa fa-fw fa-book" title='Conteúdo'></i></a>
-            <a href='#'><i className ="fa fa-fw fa-map" title='Mapa Mental'></i></a>
-        </div>
-        
+            {/* navbar como btn sanduíche(toggler) no header da página */}
+            <nav className="navbar navbar-dark bg-dark d-sm-none">
+                <div className="container-fluid">
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#mobileSidebar"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                </div>
+            </nav>
+
+            {/* navbar exposta no mobile*/}
+            <div
+                className="offcanvas offcanvas-start"
+                tabIndex="-1"
+                id="mobileSidebar"
+            >
+
+                <div className="offcanvas-header">
+
+                    <h5>Historiando</h5>
+
+                    <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="offcanvas"
+                    ></button>
+
+                </div>
+
+                <div className="offcanvas-body">
+
+                    <Link className="nav-link mb-3"  to="/" >
+                        <i className="fa fa-fw fa-home"></i> Home
+                    </Link>
+
+                    <a className="nav-link mb-3" href="#">
+                        <i className="fa fa-fw fa-search"></i> Pesquisar
+                    </a>
+
+                    <a className="nav-link mb-3" href="#">
+                        <i className="fa fa-fw fa-question"></i> Perguntas
+                    </a>
+
+                    <a className="nav-link mb-3" href="#">
+                        <i className="fa fa-fw fa-book"></i> Conteúdo
+                    </a>
+
+                    <a className="nav-link" href="#">
+                        <i className="fa fa-fw fa-map"></i> Mapa Mental
+                    </a>
+
+                </div>
+
+            </div>
+
+            {/* navbar em dimensões desktop */}
+            <div className="sidenav d-none d-sm-block"> {/*esse d-sm-block significa que em dimensões small a navbar vai conseguir se tornar um "bloco" maleável*/}
+
+                <Link to="/">
+                    <i className="fa fa-fw fa-home" title="Home"></i>
+                </Link>
+
+                <a href="#">
+                    <i className="fa fa-fw fa-search" title="Pesquisar"></i>
+                </a>
+
+                <a href="#">
+                    <i className="fa fa-fw fa-question" title="Perguntas"></i>
+                </a>
+
+                <a href="#">
+                    <i className="fa fa-fw fa-book" title="Conteúdo"></i>
+                </a>
+
+                <a href="#">
+                    <i className="fa fa-fw fa-map" title="Mapa Mental"></i>
+                </a>
+
+            </div>
         </>
-    )
-
+    );
 }
 
 export default SideNavigationBar;
