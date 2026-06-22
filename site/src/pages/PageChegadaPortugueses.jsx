d604a01f0a82f128dba7c01bc1241b8abb26df10
maju2107 <maju.mariajuliaaraujo@gmail.com>
2026-06-21 22:19:57 -0300

    [update] PageChegada responsiva

--- site/src/pages/PageChegadaPortugueses.jsx
+++ site/src/pages/PageChegadaPortugueses.jsx
@@ -7,13 +7,17 @@ function PageChegadaPortugueses() {
 
     return (
         <>
-        <h1> {content.title} </h1>
-
-        {content.paragraphs.map( (paragraph) => (
-            <div className='paragraph' key={paragraph.id}>
-                <p>{paragraph.text}</p>
+            <div className='container'>
+                <div className='content-page'>
+                    <h1 className='text-center mb-5'> {content.title} </h1>
+                        {content.paragraphs.map( (paragraph) => (
+                            <div className='paragraph mb-4' key={paragraph.id}>
+                                <p>{paragraph.text}</p>
+                            </div>
+                        ))}
+                </div>
             </div>
-        ))}
+
         </>
     )
 }
